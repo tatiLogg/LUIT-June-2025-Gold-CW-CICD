d8436c890e2a65b442fdd176aab96b8b04a7e6e8
Selina Loggins <vsphere@example.com>
2025-06-07 22:57:14 -0500

    Update logs_to_cloudwatch.py

---
+++ logs_to_cloudwatch.py
@@ -0,0 +1,12 @@
+import boto3
+
+
+# read in log
+
+# create a log group if it doesnt exist
+
+# create a log stream if it doesnt exist
+
+# create log event
+
+# create log event for each line in log 
